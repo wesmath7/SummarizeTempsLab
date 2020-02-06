58bfaba5c54c84e5b568a8c32ff07d900d497561
wesmath7 <59981828+wesmath7@users.noreply.github.com>
2020-02-06 16:42:42 -0600

    thursday work

--- SummarizeTempsLab/SummarizeTempsLab/Program.cs
+++ SummarizeTempsLab/SummarizeTempsLab/Program.cs
@@ -6,7 +6,18 @@ namespace SummarizeTempsLab
     class Program
     {
         static void Main(string[] args)
-        {
+        {string filepath = "temps.txt"
+
+                if (File.Exists(filepath)) ;
+            { using (StreamReader sr = File.OpenText(filepath)) ;
+                string line = "";
+                while ((line = sr.ReadLine()) != null) ;
+                {
+
+
+                }
+
+            }
             // temperature data is in temps.txt
         }
     }
