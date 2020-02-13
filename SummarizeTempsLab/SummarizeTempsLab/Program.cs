41a14091ce28977c96f3448532edd305c4ddbd3e
wesmath7 <59981828+wesmath7@users.noreply.github.com>
2020-02-13 14:27:49 -0600

    summarizes temp info

--- SummarizeTempsLab/SummarizeTempsLab/Program.cs
+++ SummarizeTempsLab/SummarizeTempsLab/Program.cs
@@ -7,7 +7,57 @@ namespace SummarizeTempsLab
     {
         static void Main(string[] args)
         {
-            // temperature data is in temps.txt
+            string filename;
+            Console.WriteLine("Enter a filename");
+
+            filename = Console.ReadLine();
+
+            if (File.Exists(filename)) 
+            {
+                Console.WriteLine("File exist");
+                Console.WriteLine("Enter threshold");
+                string input;
+                int threshold;
+                input = Console.ReadLine();
+                threshold = int.Parse(input);
+
+                int sumtemps = 0;
+                int numabove = 0;
+                int numbelow = 0;
+                double average;
+
+                using (StreamReader sr = File.OpenText(filename))
+                {
+
+                    string line = sr.ReadLine();
+                    int temp;
+
+                    while (line != null)
+                    {
+                        temp = int.Parse(line);
+                        sumtemps += temp;
+
+                        if (temp >= threshold)
+                        {
+                            numabove += 1;
+                        }
+                        else
+                        {
+                            numbelow += 1;
+                        }
+
+                        line = sr.ReadLine();
+                    }
+                }
+                Console.WriteLine("Temperatures above " + numabove);
+                Console.WriteLine("Temperatures below " + numbelow);
+                average = sumtemps / (numabove + numbelow);
+                Console.WriteLine("The average temperature is " + average);
+            }
+            else
+            {
+                Console.WriteLine("The file does not exist");
+            }
         }
     }
 }
